654a8ee279957956f41416694a48204066c2bd3e
Kate Kondratenko <kondratenko.es@gmail.com>
2025-04-03 21:47:49 +0300

    add about-me component

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent {
-
+  headingText = "HI, I'M KATE KONDRATENKO PROFESSIONAL FRONTEND DEVELOPER";
+  leftText = "Based in Haifa, Israel and working Worldwide";
+  rightText = "Passionate Frontend Developer Crafting Visually Captivating Websites with Angular or anything you wish";
 }
