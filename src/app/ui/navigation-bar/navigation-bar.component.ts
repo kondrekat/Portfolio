ca665577a35e35117a439c798233a036e74173e8
Kate Kondratenko <kondratenko.es@gmail.com>
2025-04-17 14:39:55 +0300

    navigation for phone

--- src/app/ui/navigation-bar/navigation-bar.component.ts
+++ src/app/ui/navigation-bar/navigation-bar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.scss']
+  styleUrls: ['./navigation-bar.component.scss'],
+  imports: [
+    CommonModule // Убедитесь, что CommonModule добавлен в массив imports
+  ],
 })
 export class NavigationBarComponent {
   name = 'KATE KONDRATENKO';
@@ -15,4 +19,18 @@ export class NavigationBarComponent {
     { label: 'WORK', link: '#projects' },
     { label: 'CONTACT', link: '#get-in-touch' }
   ];
+
+  isMobileMenuOpen: boolean = false;
+
+  get combinedNavigationLinks() {
+    return [...this.navigationLinksLeft, ...this.navigationLinksRight];
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu () {
+    this.isMobileMenuOpen = false;
+  }
 }
